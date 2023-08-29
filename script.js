e97502bbc3c9d5ef03b6c635de2955bf261cc0ac
IhO17 <107650639+IhO17@users.noreply.github.com>
2023-08-30 00:23:52 +0200

    make changeSize

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
-const board = document.querySelector(".canvas");
-
-board.style.gridTemplateRows = "repeat(16, 1fr)";
-board.style.gridTemplateColumns = "repeat(16, 1fr)";
-
-for (let i = 0; i < 17; i++) {
-    const cell = document.createElement("div");
-    cell.style.backgroundColor = "blue";
-    cell.addEventListener("mousemove", () => cell.style.backgroundColor = "black");
 
 
-    board.insertAdjacentElement("beforeend", cell);
+function drawBoard(size) {
+    const board = document.querySelector(".canvas");
+    const cells = board.querySelectorAll("div");
+    cells.forEach(div => div.remove());
 
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+
+    for (let i = 0; i < size ** 2; i++) {
+        const cell = document.createElement("div");
+
+        cell.style.backgroundColor = "white";
+        cell.addEventListener("mousemove", () => cell.style.backgroundColor = "black");
+        board.insertAdjacentElement("beforeend", cell);
+    }
 
 }
+
+
 
 function changeColor() {
 
 }
 
 function changeSize() {
-    let x = prompt("Insert size:", placeholder="2-100")
+    let x = prompt("Insert size:", placeholder="value between 2-100");
+    
+    while (x < 2 || x > 100) {
+        x = prompt("Value out of range, retry:", placeholder="value between 2-100");
+    }
+    drawBoard(x);
 }
+
+drawBoard(16);
